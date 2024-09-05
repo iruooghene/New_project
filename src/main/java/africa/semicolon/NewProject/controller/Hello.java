299b04b5a76d21ba03645e20248208da37504cd6
Victoria <ebohiruooghene@gmail.com>
2024-09-05 08:53:11 +0100

    new commit

--- src/main/java/africa/semicolon/NewProject/controller/Hello.java
+++ src/main/java/africa/semicolon/NewProject/controller/Hello.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class Hello {
     @GetMapping("/newProject")
     public String hi(){
-        return "i am connected";
+        return "i am connected!!!!!";
     }
 
 
